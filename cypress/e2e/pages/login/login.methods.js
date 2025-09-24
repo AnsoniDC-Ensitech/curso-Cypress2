da2ee4e7829944f572d079dd5595ba6733db6a12
AnsoniDC-Ensitech <ansoni@ensitech.com>
2025-09-23 18:07:23 -0600

    confi 23092025

---
+++ cypress/e2e/pages/login/login.methods.js
@@ -0,0 +1,21 @@
+import { use } from "react";
+import { LoginElements } from "./login.elements";
+
+export class LoginMethos{
+    static insertUsername(username){
+        LoginElements.textboxes.username.type(username)
+    }
+    static insertPassword(password){
+        LoginElements.textboxes.password.type(password)
+    }
+
+    static clickOnLoginButton(){
+        LoginElements.buttons.login.click()
+    }
+
+    static login (username, password){
+        this.insertUsername(username)
+        this.insertPassword(password)
+        this.clickOnLoginButton()
+    }
+}
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,11 @@
+import { LoginElements } from "./pages/login/login.elements"
+import { LoginMethos } from "./pages/login/login.methods"
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://demoblaze.com/')
+    cy.get('a[data-target="#logInModal"]').click()
+    LoginMethos.login('username','password')
+    cy.wait(5000)
   })
 })
